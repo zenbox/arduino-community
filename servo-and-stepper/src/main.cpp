5ce3a67ffba24a37fc7b8357b06ddc5b2c3b8cb6
supergenehm <joeimperator@JoeImpeatorsAir.fritz.box>
2017-01-19 15:11:30 +0100

    port changed

--- servo-and-stepper/src/main.cpp
+++ servo-and-stepper/src/main.cpp
@@ -25,7 +25,7 @@ const byte PIN = 13;
  *       Most servos needs an angle between 0 and 180 degree.
  *       The servo library is an Arduino standard lib.
  */
-const byte SERVO_PIN = 9;
+const byte SERVO_PIN = 3;
 Servo servo_1;
 int servoAngle;
 
@@ -34,40 +34,40 @@ const byte POTENTIOMETER_PIN = A3;
 int potentiometerValue = 0;
 
 void setup() {
-	// init a serial port
-	Serial.begin(9600);
-	Serial.print("Everything seems fine ...");
+								// init a serial port
+								Serial.begin(9600);
+								Serial.print("Everything seems fine ...");
 
-	// init the led pin
-	pinMode(PIN, OUTPUT);
-	digitalWrite(PIN, LOW);
+								// init the led pin
+								pinMode(PIN, OUTPUT);
+								digitalWrite(PIN, LOW);
 
-	/**
-	 * @desc init the servo with the dedicated pin.
-	 *       Alternatively with pin, startAngle, endAngle
-	 *       attach(p, sa, ea).
-	 *       The write() method send an angle to turn to.
-	 */
-	servo_1.attach(9);
-	servo_1.write(servoAngle);
+								/**
+								 * @desc init the servo with the dedicated pin.
+								 *       Alternatively with pin, startAngle, endAngle
+								 *       attach(p, sa, ea).
+								 *       The write() method send an angle to turn to.
+								 */
+								servo_1.attach(9);
+								servo_1.write(servoAngle);
 
-	// init the potentiometer
-	potentiometerValue = analogRead(POTENTIOMETER_PIN);
+								// init the potentiometer
+								potentiometerValue = analogRead(POTENTIOMETER_PIN);
 }
 
 void loop() {
-	/**
-	 *  @desc stearing the servo means i.e. easily mapping
-	 *        the potentiometer values to a servo angle
-	 *        and just send them.
-	 */
-	potentiometerValue = analogRead(POTENTIOMETER_PIN);
-	servoAngle = map(potentiometerValue, 0,1023, 0,180);
-	servo_1.write(servoAngle);
+								/**
+								 *  @desc stearing the servo means i.e. easily mapping
+								 *        the potentiometer values to a servo angle
+								 *        and just send them.
+								 */
+								potentiometerValue = analogRead(POTENTIOMETER_PIN);
+								servoAngle = map(potentiometerValue, 0,1023, 0,180);
+								servo_1.write(servoAngle);
 
-	// blink
-	digitalWrite(PIN, HIGH);
-	delay(500);
-	digitalWrite(PIN, LOW);
-	delay(500);
+								// blink
+								digitalWrite(PIN, HIGH);
+								delay(500);
+								digitalWrite(PIN, LOW);
+								delay(500);
 }
