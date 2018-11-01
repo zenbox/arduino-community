ddcde8b20314d700c01dec7e3fc03b273e3d69f1
zenbox <michael@zenbox.de>
2018-11-01 14:49:44 +0100

    läuft auf UNO

--- Lightsabermathis/Lightsabermathis.ino
+++ Lightsabermathis/Lightsabermathis.ino
@@ -1,92 +1,112 @@
+/**
+   Lichtschwert by Mathis
+   @version 1.0.0
+   @since 2018-11-01
+   @author Mathis
 
-int ledPin1 = 0;
-int ledPin2 = 1;
-int ledPin3 = 2;
-int buttonPin = 3;
+   AT Tiny:
+   Reset -| U |- 5V
+   A3, 3 -|   |- 2, A1
+   A2, 4 -|   |- 1 (PWM)
+   GND   -|   |- 0 (PWM)
+*/
+// for Tiny
+//int ledPin1 = 0;
+//int ledPin2 = 1;
+//int ledPin3 = 2;
+//int buttonPin = 3;
+//int powbuttonPin = 4;
+
+// for UNO
+int ledPin1 = 2;
+int ledPin2 = 3;
+int ledPin3 = 4;
+int buttonPin = 5;
+int powbuttonPin = 6;
+
 int buttonState = 0;
-int state = 0 ;
-int hasChanged = 1;
-int powbuttonPin = 4;
-int powbuttonValue = 0;
+int state = 0;
+int powbuttonState = 0;
 int power = 0;
-int on = 0;
+
+int delayZeit = 150;
 
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("started");
+
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(powbuttonPin, INPUT);
-
-
 }
 
 void loop() {
+  // - - - - - - - - - -
+  // read button
+  buttonState = digitalRead(buttonPin);
+  powbuttonState = digitalRead(powbuttonPin);
+
+  // - - - - - - - - - -
+  // color button states
+  if (buttonState == HIGH) {
+    state = state + 1;
+
+    if (state > 2) {
+      state = 0;
+    }
+
+    Serial.print("state is ");
+    Serial.print(state);
+    Serial.println();
+  }
+
+  // - - - - - - - - - -
+  // power button states
+  if (powbuttonState == HIGH ) {
+    if (power == 0) {
+      power = 1;
+    } else {
+      power = 0;
+    }
+
+    Serial.print("power is ");
+    Serial.print(power);
+    Serial.println();
+
+  }
+
+  // Control: was passiert ...
 
   if (power == 0) {
     digitalWrite(ledPin1, LOW);
     digitalWrite(ledPin2, LOW);
     digitalWrite(ledPin3, LOW);
+    delay(delayZeit);
 
-  }
-  buttonState = digitalRead(buttonPin);
-  powbuttonValue = digitalRead(powbuttonPin);
-  delay(150);
-
-  if (powbuttonValue == 1 ) {
-    if (on == 0) {
-      power = power + 1;
-      on = on + 1;
-      delay(80);
-    }
-    else if (on == 1) {
-      power = 0;
-      on = 0;
-    }
-  }
-
-
-
-
-
-  if (buttonState == HIGH) {
-    state = state + 1;
-  }
-
-  if (state == 4) {
-    state = 0;
-  }
-
-
-  if (power == 1) {
+  } else {
     switch (state) {
       case 0:
         digitalWrite(ledPin1, HIGH);
         digitalWrite(ledPin2, LOW);
         digitalWrite(ledPin3, LOW);
-        hasChanged = 0;
-
         break;
 
       case 1:
         digitalWrite(ledPin1, LOW);
         digitalWrite(ledPin2, HIGH);
         digitalWrite(ledPin3, LOW);
-        hasChanged = 0;
-
         break;
 
       case 2:
         digitalWrite(ledPin1, LOW);
         digitalWrite(ledPin2, LOW);
         digitalWrite(ledPin3, HIGH);
-        hasChanged = 1;
-
         break;
     }
+    delay(delayZeit);
+
   }
 }
-
-
-
