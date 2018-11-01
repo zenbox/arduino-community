58b11ef9501daa54fba3f2b2f03a0c262620b64a
zenbox <michael@zenbox.de>
2018-11-01 12:20:31 +0100

    Comment mit Autor

--- lichtschwerttiny/lichtschwerttiny.ino
+++ lichtschwerttiny/lichtschwerttiny.ino
@@ -1,3 +1,9 @@
+/**
+ * Lichtschwert by Linus
+ * @version 1.0.0
+ * @since 2018-11-01
+ * @author Linus
+ */
 int ledR = 9;
 int ledG = 11;
 int ledB = 10;
@@ -12,62 +18,61 @@ int zeit = 100;
 int on = 0;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(ledR, OUTPUT);
-  pinMode(ledG, OUTPUT);
-  pinMode(ledB, OUTPUT);
-  pinMode(button, INPUT);
-  pinMode(powerbutton, INPUT);
+        Serial.begin(9600);
+        pinMode(ledR, OUTPUT);
+        pinMode(ledG, OUTPUT);
+        pinMode(ledB, OUTPUT);
+        pinMode(button, INPUT);
+        pinMode(powerbutton, INPUT);
 }
 
 void loop() {
-  buttonstate = digitalRead(button);
-  powerbuttonstate = digitalRead(powerbutton);
-  if (buttonstate == HIGH)
-    state = state + 1;
-  delay(zeit);
-  if (state > 3) {
-    state = 1;
-  }
-  if (powerbuttonstate == HIGH ) {
-    if (on == 0) {
-      power = 1;
-      on = 1;
-      delay(80);
-    }
-    else if (on == 1) {
-      power = 0;
-      on = 0;
-      delay(80);
-    }
-  }
-  if (power == 0){
-    analogWrite(ledR, LOW);
-    analogWrite(ledG, LOW);
-    analogWrite(ledB, LOW);
-  }
+        buttonstate = digitalRead(button);
+        powerbuttonstate = digitalRead(powerbutton);
+        if (buttonstate == HIGH)
+                state = state + 1;
+        delay(zeit);
+        if (state > 3) {
+                state = 1;
+        }
+        if (powerbuttonstate == HIGH ) {
+                if (on == 0) {
+                        power = 1;
+                        on = 1;
+                        delay(80);
+                }
+                else if (on == 1) {
+                        power = 0;
+                        on = 0;
+                        delay(80);
+                }
+        }
+        if (power == 0) {
+                analogWrite(ledR, LOW);
+                analogWrite(ledG, LOW);
+                analogWrite(ledB, LOW);
+        }
 
-  if (power == 1) {
+        if (power == 1) {
 
-    
-    switch (state) {
-      case 1:
-        digitalWrite(ledR, HIGH);
-        digitalWrite(ledG, LOW);
-        digitalWrite(ledB, LOW);
-        break;
-      case 2:
-        digitalWrite(ledR, LOW);
-        digitalWrite(ledG, HIGH);
-        digitalWrite(ledB, LOW);
-        break;
-      case 3:
-        digitalWrite(ledR, LOW);
-        digitalWrite(ledG, LOW);
-        digitalWrite(ledB, HIGH);
-        break;
-    }
-    delay(zeit);
-  } 
+
+                switch (state) {
+                case 1:
+                        digitalWrite(ledR, HIGH);
+                        digitalWrite(ledG, LOW);
+                        digitalWrite(ledB, LOW);
+                        break;
+                case 2:
+                        digitalWrite(ledR, LOW);
+                        digitalWrite(ledG, HIGH);
+                        digitalWrite(ledB, LOW);
+                        break;
+                case 3:
+                        digitalWrite(ledR, LOW);
+                        digitalWrite(ledG, LOW);
+                        digitalWrite(ledB, HIGH);
+                        break;
+                }
+                delay(zeit);
+        }
 }
-
